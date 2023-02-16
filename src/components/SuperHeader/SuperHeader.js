4ca3f093b4e496344e9ddbc1983b886242b9cf8c
matthias.paulitsch <matthias.paulitsch@e-mundo.de>
2023-02-16 13:38:40 +0100

    ex-1: implemented ex-1

--- src/components/SuperHeader/SuperHeader.js
+++ src/components/SuperHeader/SuperHeader.js
@@ -26,10 +26,17 @@ const Wrapper = styled.div`
   font-size: 0.875rem;
   color: ${COLORS.gray[300]};
   background-color: ${COLORS.gray[900]};
+  display: flex;
+  gap: 24px;
+  padding-block: 12px;
+  padding-inline: 32px;
+  justify-content: flex-end;
 `;
 
 const MarketingMessage = styled.span`
   color: ${COLORS.white};
+  position: absolute;
+  left: 32px;
 `;
 
 const HelpLink = styled.a`
